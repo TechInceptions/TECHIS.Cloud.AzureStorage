ef902be4afdac53e797331feb69a2fd9ab16eb22
Arinze Ufoeze <arinze@techinceptions.com>
2017-05-08 16:18:19 -0400

    Added Net462 tests

---
+++ TestConsoleApp1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TECHIS.Cloud.AzureStorage;
+using Test.Cloud.AzureStorage;
+
+namespace TestConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("STart");
+            var data = ReadData462();
+            Console.WriteLine(data);
+
+            Console.WriteLine("Hit enter to exit ...");
+            Console.ReadLine();
+        }
+        public static string ReadData462()
+        {
+            BlobReader br = new BlobReader();
+            var containerUri = Connector.GetContainerUri();
+            var fileName = FixedFileName;
+            string data = null;
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                br.Connect(containerUri).ReadData(fileName, ms);
+
+                data = new string(br.Encoding.GetChars(ms.ToArray()));
+            }
+            return data;
+        }
+
+        private static string FixedFileName => "FixedDirectory/S2715H.inf";
+        private static string NonExistingFileName => "FD/43CEAA72B980FA1D305A.txt";
+
+    }
+}
